12dd64f60fd18bfc8f049bbaa0aea8e94bef7fc2
Eiling Fernandez <eilingfernan@gmail.com>
2012-11-16 15:41:12 -0800

    Milestones class
    Age picker shows and hides with done button

---
+++ MedApp/MedApp/Milestones.h
@@ -0,0 +1,33 @@
+//
+//  Milestones.h
+//  MedApp
+//
+//  Created by Eiling Fernandez on 11/15/12.
+//  Copyright (c) 2012 Ryan Huynh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Milestones : UIViewController
+
+<UIPickerViewDataSource, UIPickerViewDelegate>
+{
+    
+    IBOutlet UIPickerView *agePicker;
+    NSArray *ageData;
+    
+}
+
+@property(nonatomic, retain) UIPickerView *agePicker;
+
+@property(nonatomic, retain) NSArray *ageData;
+
+@property (weak, nonatomic) IBOutlet UITextField *ageTextField;
+
+@property (weak, nonatomic) IBOutlet UIView *pickerViewContainer;
+
+- (IBAction)showAge:(id)sender;
+- (IBAction)pickerDone:(id)sender;
+
+
+@end
