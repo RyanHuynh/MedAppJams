81f4dad9ddffbd27b1cb57ba884b5cad44f5f80c
Eiling Fernandez <eilingfernan@gmail.com>
2012-11-16 16:53:44 -0800

    test 2

---
+++ MedApp/Milestones.h
@@ -0,0 +1,13 @@
+//
+//  Milestones.h
+//  MedApp
+//
+//  Created by Eiling Fernandez on 11/16/12.
+//  Copyright (c) 2012 Ryan Huynh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Milestones : UIViewController
+
+@end
